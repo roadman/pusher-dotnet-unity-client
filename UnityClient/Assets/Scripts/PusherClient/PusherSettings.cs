a9c9643a97b534ac749d08375235d88801ee157f
Mike <MikeToString@gmail.com>
2014-05-06 16:13:46 -0400

    add static PusherSettings class

---
+++ UnityClient/Assets/Scripts/PusherClient/PusherSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PusherSettings
+{
+	// if true, Pusher will output debug info to console
+	public static bool Verbose = true;
+
+	// App Key from pusher.com app settings
+	public static string AppKey = "";
+
+	// if specified, then this will be used as callback url for authorizing connections to private channels
+	public static string HttpAuthUrl = "";
+
+	// client name & version for identifying client library
+	public static string ClientName = "pusher-unity";
+	public static string ClientVersion = "1.0";
+}
