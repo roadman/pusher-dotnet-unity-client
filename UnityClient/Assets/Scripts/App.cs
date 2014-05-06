c7bd851b205a4766bbbd3f301ef06d326771f740
Mike <MikeToString@gmail.com>
2014-05-06 16:15:04 -0400

    bare bones example App that connects to a private channel, listens for messages, and outputs debug information to log file

--- UnityClient/Assets/Scripts/App.cs
+++ UnityClient/Assets/Scripts/App.cs
@@ -2,14 +2,43 @@
 using System.Collections;
 
 public class App : MonoBehaviour {
+	// isntance of pusher client
+	PusherClient.Pusher pusherClient = null;
+	PusherClient.Channel pusherChannel = null;
 
-	// Use this for initialization
+	// Initialize
 	void Start () {
-	
+		// TODO: Replace these with your app values
+		PusherSettings.Verbose = true;
+		PusherSettings.AppKey = "";
+		PusherSettings.HttpAuthUrl = "http://richmond.kingdoms.metamoki.com/bin/pusher-auth.php";
+
+		PusherClient.PusherOptions opts = new PusherClient.PusherOptions();
+		opts.Encrypted = true;
+		opts.Authorizer = new PusherClient.HttpAuthorizer( PusherSettings.HttpAuthUrl );
+
+		pusherClient = new PusherClient.Pusher( PusherSettings.AppKey, opts );
+		pusherClient.Connected += HandleConnected;
+		pusherClient.ConnectionStateChanged += HandleConnectionStateChanged;
+		pusherClient.Connect();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void HandleConnected (object sender) {
+		Debug.Log ( "Pusher client connected, now subscribing to private channel" );
+		pusherChannel = pusherClient.Subscribe( "private-testchannel" );
+		pusherChannel.BindAll( HandleChannelEvent );
+	}
+
+	void OnDestroy() {
+		if( pusherClient != null )
+			pusherClient.Disconnect();
+	}
+
+	void HandleChannelEvent( string eventName, object evData ) {
+		Debug.Log ( "Received event on channel, event name: " + eventName + ", data: " + JsonHelper.Serialize(evData) );
+	}
+
+	void HandleConnectionStateChanged (object sender, PusherClient.ConnectionState state) {
+		Debug.Log ( "Pusher connection state changed to: " + state );
 	}
 }
